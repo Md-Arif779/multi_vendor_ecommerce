5783613ab72bac2030a66bb53597ff46441c65a6
md Arif <mdarifcse997@gmail.com>
2024-03-27 19:32:53 +0600

    order item in cart

--- calc/urls.py
+++ calc/urls.py
@@ -1,6 +1,22 @@
 
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet, CartViewSet, CartItemViewSet, OrderViewSet, DailyDataViewSet
+
+
+
+from .views import ProductViewSet, CartViewSet, CartItemViewSet, OrderViewSet, DailyDataViewSet
+
+
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
+router.register(r'carts', CartViewSet)
+router.register(r'cart-items', CartItemViewSet)
+
+router.register(r'daily-data', DailyDataViewSet)
 
 urlpatterns = [
-    
+    path('', include(router.urls)),
+    path('orders/', OrderViewSet.as_view({'post': 'create'}), name='order-create'), 
 ]
--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Product, Cart, CartItem, Order, DailyData
+from .serializers import ProductSerializer, CartSerializer, CartItemSerializer, OrderSerializer, DailyDataSerializer
 # Create your views here.
+
+
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class CartViewSet(viewsets.ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+
+class CartItemViewSet(viewsets.ModelViewSet):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+
+class OrderViewSet(viewsets.ViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def create(self, request):
+        cart_items = request.user.cart.cartitem_set.all()
+        if cart_items.exists():
+            order = Order.objects.create(user=request.user, total_amount=0)  # Initialize order
+            total_amount = 0
+            for cart_item in cart_items:
+                order.items.add(cart_item)
+                total_amount += cart_item.product.price * cart_item.quantity
+            order.total_amount = total_amount
+            order.save()
+            request.user.cart.cartitem_set.all().delete()  # Empty the cart
+            return Response(OrderSerializer(order).data, status=status.HTTP_201_CREATED)
+        else:
+            return Response({'detail': 'No items in the cart'}, status=status.HTTP_400_BAD_REQUEST)
+
+class DailyDataViewSet(viewsets.ModelViewSet):
+    queryset = DailyData.objects.all()
+    serializer_class = DailyDataSerializer
